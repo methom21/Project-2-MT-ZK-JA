7e780adf4222dc22cf10e5a479d7d05e60b6c077
Joseph Amador <kirito.1986.714@gmail.com>
2021-07-08 11:11:56 -0700

    added api routes and directories

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const heroApiRoutes = require('./heroApi');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/heroApi', heroApiRoutes);
+
 
 module.exports = router;
