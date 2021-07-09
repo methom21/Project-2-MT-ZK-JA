46148be0c441a0b2d567221c17e4c5389477482d
Joseph Amador <kirito.1986.714@gmail.com>
2021-07-09 09:34:28 -0700

    added fetch mock up code.

--- controllers/heroApi/heroRoutes.js
+++ controllers/heroApi/heroRoutes.js
@@ -1,8 +1,24 @@
 // const router = require('express').Router();
 // const { User } = require('../../models');
 
+const getHero = function (hero) {
+    const name = name.trim()
+    const apiUrl = `https://superheroapi.com/api/4085762414794326/search/${hero}`;
+    return fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+           
+            return displayHero(data);
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert("Unable to gather Hero data!");
+      });
 
 
 
-
-// module.exports = router;
+module.exports = router;
