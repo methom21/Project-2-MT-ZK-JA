19f17efc150bc0c8ac2690fc22a58146033f9a82
Joseph Amador <kirito.1986.714@gmail.com>
2021-07-08 21:45:03 -0700

    we got the singup sheet to proparly append users and log in and sign in at will

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,12 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
+  // Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
+    // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
@@ -12,9 +14,10 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
+      // If successful, redirect the browser to the profile page
       document.location.replace('/');
     } else {
-      alert('Failed to log in');
+      alert(response.statusText);
     }
   }
 };
@@ -34,7 +37,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
@@ -42,9 +45,9 @@ const signupFormHandler = async (event) => {
 };
 
 document
-.querySelector('.login-form')
-.addEventListener('submit', loginFormHandler);
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
 
 document
-.querySelector('.signup-form')
-.addEventListener('submit', signupFormHandler);
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
