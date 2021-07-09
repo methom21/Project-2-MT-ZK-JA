8ae2b8e440eba50b88fc1a515312f810df89a756
Zachary K <zachwkrause@gmail.com>
2021-07-09 09:38:05 -0700

    Joe Mike and zach colab 730am-930am

---
+++ controllers/heroApi/heroCard.js
@@ -0,0 +1,15 @@
+const heroSearch = document.getElementById("#heroSearch")
+
+
+function heroSearch(input) {
+    
+}
+
+const response = await fetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({ name, email, password }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+
+  heroSearch.addEventListener("submit", heroSearch(input))
