557016bdb830c537ceb8045263c80f53a85a8bf1
Joseph Amador <kirito.1986.714@gmail.com>
2021-07-10 14:08:19 -0700

    added href

--- controllers/api/heroRoutes.js
+++ controllers/api/heroRoutes.js
@@ -6,8 +6,41 @@ const axios = require('axios');
 router.get(`/:hero`,withAuth, async (req,res)=>{
     const heroName = req.params.hero;
     const heroData = await axios.get(`https://superheroapi.com/api/4085762414794326/search/${heroName}`);
-    res.json(heroData.data.results[0])
+    res.json(heroData.data.results[0].name)
+    console.log(heroData.data.results[0].name);
+    res.append(document.getElementById('#heroName').innerHTML(heroData.data.results[0].name));
+    // console.log(weathData.list);
+  
+    // for (let i = 0; i < 5; i++) {
+    //   let iconID = weathData.list[i].weather[0].icon;
+    //   let iconUrl = `https://openweathermap.org/img/wn/${iconID}@2x.png`;
+    //   let iconEl = $("<img>").attr("alt", "weatherIcon").attr("src", iconUrl);
+  
+    //   $("#weatherIcon" + (i + 1)).append(iconEl);
+    //   $("#tempDisplay" + (i + 1)).text(weathData.list[i].main.feels_like);
+    //   $("#windDisplay" + (i + 1)).text(weathData.list[i].wind.speed);
+    //   $("#humidDisplay" + (i + 1)).text(weathData.list[i].main.humidity);
+    // }
+      
 })
+
+
+// actual route /api/heros/
+router.post(`/:hero`, withAuth, async (req, res) => {
+    try 
+    {
+        const heroName = req.params.hero;
+        const heroData = await axios.get(`https://superheroapi.com/api/4085762414794326/search/${heroName}`);
+        res.json(heroData.data.results[0].name)
+        console.log(heroData.data.results[0].name);
+        res.append("#heroName");
+        console.log("#heroName");
+    } 
+    catch (err) 
+    {
+    res.status(400).json(err);
+    }
+    });
 
 
 
