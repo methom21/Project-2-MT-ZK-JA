b038028ea0264a658992769b00089c766583f43e
methom21 <mikey.thompson8@gmail.com>
2021-07-10 10:07:35 -0700

    added rosters.js and roster.handlebars

--- public/js/roster.js
+++ public/js/roster.js
@@ -1,6 +1,14 @@
 // search db for all cards registered to active user
 // manage button listener runs delete or sideline option
-
+const handlebars = require('express-handlebars');
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+    layoutsDir: `${__dirname}/views/layouts`
+}));
+app.use(express.static(`public`));
+app.get('/',(req, res)=>{
+    res.render('main', {layout: 'roster'});
+});
 
 // Retrieve the template data from the HTML (jQuery is used here).
 var template = document.getElementById('#handlebars-demo').html();
